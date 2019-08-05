70ddb1d30089d37f59ea5efaa8558dcf4543d913
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-08-05 16:47:36 +0200

    Add Vector extensions and operators

---
+++ src/main/kotlin/com/github/johnnyjayjay/spiglin/Vector.kt
@@ -0,0 +1,42 @@
+package com.github.johnnyjayjay.spiglin
+
+import org.bukkit.util.Vector
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun Vector.abs() =
+    sqrt(x.pow(2) + y.pow(2) + z.pow(2))
+
+operator fun Vector.times(scalar: Number) =
+    clone().multiply(scalar.toDouble())
+
+operator fun Vector.timesAssign(scalar: Number){
+    multiply(scalar.toDouble())
+}
+
+operator fun Vector.plus(vector: Vector) =
+    clone().add(vector)
+
+operator fun Vector.plusAssign(vector: Vector) {
+    add(vector)
+}
+
+operator fun Vector.minus(vector: Vector) =
+    clone().subtract(vector)
+
+operator fun Vector.minusAssign(vector: Vector) {
+    subtract(vector)
+}
+
+operator fun Vector.times(vector: Vector) =
+    clone().dot(vector)
+
+operator fun Vector.timesAssign(vector: Vector) {
+    dot(vector)
+}
+
+infix fun Vector.x(vector: Vector) =
+    clone().crossProduct(vector)
+
+
+
