20efbb125fcf9cc7fce970922fca3db820ed125a
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-08-14 21:27:21 +0200

    Optimise imports

--- src/main/kotlin/com/github/johnnyjayjay/spiglin/GeneralExtensions.kt
+++ src/main/kotlin/com/github/johnnyjayjay/spiglin/GeneralExtensions.kt
@@ -2,10 +2,6 @@ package com.github.johnnyjayjay.spiglin
 
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
-import org.bukkit.event.Event
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.plugin.Plugin
 
 /**
  * @see Bukkit.getOnlinePlayers
