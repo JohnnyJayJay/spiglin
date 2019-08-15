29ae6fb1c58b1ac3ff007ebbe9c20d0b6130bfb0
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-08-15 17:34:33 +0200

    change 'hear()' implementation again (uses own anonymous objects now)

--- src/main/kotlin/com/github/johnnyjayjay/spiglin/event/ListenerExtensions.kt
+++ src/main/kotlin/com/github/johnnyjayjay/spiglin/event/ListenerExtensions.kt
@@ -2,6 +2,7 @@ package com.github.johnnyjayjay.spiglin.event
 
 import org.bukkit.Bukkit
 import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.plugin.Plugin
 import java.util.concurrent.TimeUnit
@@ -10,8 +11,13 @@ fun Plugin.register(listener: Listener) {
     Bukkit.getPluginManager().registerEvents(listener, this)
 }
 
-inline fun <reified T : Event> hear(noinline action: (T) -> Unit) {
-    expect(amount = ExpectedEvent.INDEFINITE_AMOUNT, action = action)
+inline fun <reified T : Event> Plugin.hear(crossinline action: (T) -> Unit) {
+    register(object : Listener {
+        @EventHandler
+        fun onEvent(event: T) {
+            action(event)
+        }
+    })
 }
 
 inline fun <reified T : Event> expect(
