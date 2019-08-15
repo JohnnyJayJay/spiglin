9b474b187be2a3ec1755b4b80cb419e2fc74bf77
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-08-15 17:27:06 +0200

    Add EntityDamageByEntityEvent to ItemSubjectListener

--- src/main/kotlin/com/github/johnnyjayjay/spiglin/interaction/ItemSubjectListener.kt
+++ src/main/kotlin/com/github/johnnyjayjay/spiglin/interaction/ItemSubjectListener.kt
@@ -1,10 +1,12 @@
 package com.github.johnnyjayjay.spiglin.interaction
 
 import com.github.johnnyjayjay.spiglin.inventory.get
+import org.bukkit.entity.LivingEntity
 import org.bukkit.event.Event
 import org.bukkit.event.block.BlockDropItemEvent
 import org.bukkit.event.enchantment.EnchantItemEvent
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDropItemEvent
 import org.bukkit.event.entity.EntityPickupItemEvent
 import org.bukkit.event.entity.ItemMergeEvent
@@ -33,6 +35,16 @@ object ItemSubjectListener : GenericSubjectListener<ItemStack>() {
                     add(inventory[event.previousSlot])
                     add(inventory[event.newSlot])
                 }
+                is EntityDamageByEntityEvent -> {
+                    val damager = event.damager
+                    if (damager is LivingEntity) {
+                        add(damager.equipment?.itemInHand)
+                    }
+                    val victim = event.entity
+                    if (victim is LivingEntity) {
+                        addAll(victim.equipment?.armorContents ?: emptyArray())
+                    }
+                }
                 is EntityPickupItemEvent -> add(event.item.itemStack)
                 is EntityDropItemEvent -> add(event.itemDrop.itemStack)
                 is ItemMergeEvent -> add(event.entity.itemStack)
@@ -53,6 +65,7 @@ object ItemSubjectListener : GenericSubjectListener<ItemStack>() {
                 }
                 else -> {}
             }
+            Unit
         }
         return items.filterNotNull()
     }
