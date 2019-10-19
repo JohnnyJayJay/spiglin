f5d2e2186840a0c67022730b9f97ff3988522a3b
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-10-19 18:57:25 +0200

    Remove illogical location maths

--- src/main/kotlin/com/github/johnnyjayjay/spiglin/LocationExtensions.kt
+++ src/main/kotlin/com/github/johnnyjayjay/spiglin/LocationExtensions.kt
@@ -21,17 +21,3 @@ operator fun Location.minusAssign(vector: Vector) {
     subtract(vector)
 }
 
-operator fun Location.times(number: Number) =
-    clone().multiply(number.toDouble())
-
-operator fun Location.timesAssign(number: Number) {
-    multiply(number.toDouble())
-}
-
-operator fun Location.div(number: Number) =
-    clone().multiply(1 / number.toDouble())
-
-operator fun Location.divAssign(number: Number) {
-    multiply(1 / number.toDouble())
-}
-
