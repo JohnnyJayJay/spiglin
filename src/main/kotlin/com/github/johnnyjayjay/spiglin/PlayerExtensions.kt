6fc6624ff0d47d2b336be832e644ee8e81944da9
JohnnyJayJay <johnnyjayjay02@gmail.com>
2019-10-19 16:54:52 +0200

    Add show extension functions in player extensions, enable optional usage of new API

--- src/main/kotlin/com/github/johnnyjayjay/spiglin/PlayerExtensions.kt
+++ src/main/kotlin/com/github/johnnyjayjay/spiglin/PlayerExtensions.kt
@@ -3,6 +3,7 @@ package com.github.johnnyjayjay.spiglin
 import org.bukkit.*
 import org.bukkit.entity.Damageable
 import org.bukkit.entity.Player
+import org.bukkit.plugin.Plugin
 
 /** Sets this Damageable's health to 0. */
 fun Damageable.kill() {
@@ -20,21 +21,44 @@ fun Player.feed() {
     foodLevel = 20
 }
 
+/** Shows this player to the given players */
+@Suppress("DEPRECATION")
+fun Player.showTo(vararg players: Player, plugin: Plugin? = null) =
+    players.forEach { if (plugin != null) it.showPlayer(plugin, this) else it.showPlayer(this) }
+
+/** Shows this player to the given players */
+@Suppress("DEPRECATION")
+fun Player.showTo(players: Iterable<Player>, plugin: Plugin? = null) =
+    players.forEach { if (plugin != null) it.showPlayer(plugin, this) else it.showPlayer(this) }
+
+/** Shows this player to all [onlinePlayers] that match the given predicate. */
+@Suppress("DEPRECATION")
+fun Player.showIf(predicate: (Player) -> Boolean, plugin: Plugin? = null) {
+    onlinePlayers.asSequence()
+        .filter(predicate)
+        .forEach { if (plugin != null) it.showPlayer(plugin, this) else it.showPlayer(this) }
+}
+
+/** Shows this player to all [onlinePlayers]. */
+fun Player.showToAll() =
+    showTo(onlinePlayers)
+
 /** Hides this player from the given players.*/
 @Suppress("DEPRECATION")
-fun Player.hideFrom(vararg players: Player) =
-    players.forEach { it.hidePlayer(this) }
+fun Player.hideFrom(vararg players: Player, plugin: Plugin? = null) =
+    players.forEach { if (plugin != null) it.hidePlayer(plugin, this) else it.hidePlayer(this) }
 
 /** Hides this player from the given players. */
 @Suppress("DEPRECATION")
-fun Player.hideFrom(players: Iterable<Player>) =
-    players.forEach { it.hidePlayer(this) }
+fun Player.hideFrom(players: Iterable<Player>, plugin: Plugin? = null) =
+    players.forEach { if (plugin != null) it.hidePlayer(plugin, this) else it.hidePlayer(this) }
 
 /** Hides this player from all [onlinePlayers] that match the given predicate. */
-fun Player.hideIf(predicate: (Player) -> Boolean) {
+@Suppress("DEPRECATION")
+fun Player.hideIf(predicate: (Player) -> Boolean, plugin: Plugin? = null) {
     onlinePlayers.asSequence()
         .filter(predicate)
-        .forEach { this.hideFrom(it) }
+        .forEach { if (plugin != null) it.hidePlayer(plugin, this) else it.hidePlayer(this) }
 }
 
 /** Hides this player from all [onlinePlayers]. */
